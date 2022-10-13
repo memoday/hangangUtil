60d75923e1076ae7706a89ec261513b39366404b
memoday <74040890+memoday@users.noreply.github.com>
2022-10-13 17:10:44 +0900

    Update
    cntCrawl 기능추가 + 코드 정리

--- cntCrawl.py
+++ cntCrawl.py
@@ -1,22 +1,53 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from numpy import imag
 import requests
 from bs4 import BeautifulSoup
 import pyperclip
 import re
+from PyQt5.QtGui import *
+
 
 form_class = uic.loadUiType("main.ui")[0]
 print("프로그램이 구동됩니다.")
 
+def get_real_url_from_shortlink(url): #단축링크 원본링크로 변경
+    resp = requests.get(url)
+    return resp.url
+
+def checkNews(url) -> tuple : #언론사별 selector
+
+    url = get_real_url_from_shortlink(url)
+    web = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
+    source = BeautifulSoup(web.text,'html.parser')
+
+    if "n.news.naver" in url: #네이버뉴스
+        print('n.news.naver checked')
+        title = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_title > h2").text
+        press = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_top > a > img.media_end_head_top_logo_img.light_type")['alt']
+        content = source.select_one("#dic_area")
+        date = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span").text
+
+    if "sports.news.naver" in url: #네이버 스포츠뉴스
+        print('sports.news.naver checked')
+        title = source.select_one("#content > div > div.content > div > div.news_headline > h4").text
+        press = source.select_one("#pressLogo > a > img")['alt']
+        content = source.select_one("#newsEndContents")
+        date = source.select_one("#content > div > div.content > div > div.news_headline > div > span:nth-child(1)").text
+
+    else:
+        print("호환되지 않는 언론사입니다")
+    return title,press,content,date
 
 class WindowClass(QMainWindow, form_class) :
 
     def __init__(self) :
         super().__init__()
-        self.setupUi(self)
+        self.setupUi(self)     
 
         #버튼에 기능을 연결하는 코드
+        self.setWindowIcon(QIcon('HGM.ico'))
         self.btn_ok.clicked.connect(self.naverSort)
         self.btn_reset.clicked.connect(self.reset)
         self.input_link.returnPressed.connect(self.naverSort)
@@ -24,29 +55,33 @@ class WindowClass(QMainWindow, form_class) :
 
     def reset(self) :
         self.input_link.setText("")
+        self.output.setText("")
+
 
     def naverSort(self):
         global output
-        
-        url = self.input_link.text()
-        web = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
-        source = BeautifulSoup(web.text,'html.parser')
-        
-        title = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_title > h2").text
-        press = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_top > a > img.media_end_head_top_logo_img.light_type")['alt']
-        content = source.select_one("#dic_area")
-        date = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span").text
-        contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
-        contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-        contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
-        to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
-        contentOut = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
+        try:
+            url = self.input_link.text()
+            
+            title, press, content, date = checkNews(url)
 
-        print(title+"\n"+press+" "+date+"\n"+contentOut)
-        output = title+"\n"+press+" "+date+"\n"+contentOut
+            contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
+            contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
+            contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
+            to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
+            contentOut = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
 
-        self.output.setText(output)
-        print(self.output.toPlainText())
+            print(title+"\n"+press+" "+date+"\n"+contentOut)
+            output = title+"\n"+press+" "+date+"\n"+contentOut
+
+            self.output.setText(output)
+            print(self.output.toPlainText())
+        except AttributeError:
+            print("내용을 찾을 수 없습니다. 네이버 뉴스 기사가 맞는지 다시 확인해주세요.")
+            self.output.setText("내용을 찾을 수 없습니다. 네이버 뉴스 기사가 맞는지 다시 확인해주세요.")
+        except Exception :
+            print("알 수 없는 이유로 실패했습니다")
+            self.output.setText("알 수 없는 이유로 실패했습니다")
 
     def copy(self):
         content = self.output.toPlainText()
--- newsCrawl.py
+++ newsCrawl.py
@@ -1,71 +1,66 @@
-from inspect import Traceback
-import sys
-from types import TracebackType
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
 import requests
 from bs4 import BeautifulSoup
-import pyperclip
-import re
-from PyQt5.QtGui import QIcon
+from openpyxl import Workbook
 
-form_class = uic.loadUiType("main.ui")[0]
-print("프로그램이 구동됩니다.")
+wb = Workbook()
+ws1 = wb.active
 
+def croll():
 
-class WindowClass(QMainWindow, form_class) :
-
-    def __init__(self) :
-        super().__init__()
-        self.setupUi(self)
-
-        #버튼에 기능을 연결하는 코드
-        self.setWindowIcon(QIcon('HGM.ico'))
-        self.btn_ok.clicked.connect(self.naverSort)
-        self.btn_reset.clicked.connect(self.reset)
-        self.input_link.returnPressed.connect(self.naverSort)
-        self.btn_copy.clicked.connect(self.copy)
-
-    def reset(self) :
-        self.input_link.setText("")
-        self.output.setText("")
-
-    def naverSort(self):
-        global output
-        try:
-            url = self.input_link.text()
-            web = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
-            source = BeautifulSoup(web.text,'html.parser')
-            
-            title = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_title > h2").text
-            press = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_top > a > img.media_end_head_top_logo_img.light_type")['alt']
-            content = source.select_one("#dic_area")
-            date = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span").text
-            contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
-            contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-            contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
-            to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
-            contentOut = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
-
-            print(title+"\n"+press+" "+date+"\n"+contentOut)
-            output = title+"\n"+press+" "+date+"\n"+contentOut
-
-            self.output.setText(output)
-            print(self.output.toPlainText())
-        except AttributeError:
-            print("내용을 찾을 수 없습니다. 네이버 뉴스 기사가 맞는지 다시 확인해주세요.")
-            self.output.setText("내용을 찾을 수 없습니다. 네이버 뉴스 기사가 맞는지 다시 확인해주세요.")
-        except Exception :
-            print("알 수 없는 이유로 실패했습니다")
-            self.output.setText("알 수 없는 이유로 실패했습니다")
-
-    def copy(self):
-        content = self.output.toPlainText()
-        pyperclip.copy(content)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv) 
-    myWindow = WindowClass() 
-    myWindow.show()
-    app.exec_()
+    keyword = '한강'
+    currentPage = 0
+    ds = "2022.10.12" #from date(ds)
+    de = "2022.10.12" #to date(de)
+    sort = "2" #오래된 순: 2
+    pd = "3" #기간검색: 3
     
+    lastPage = 1
+    
+    # ws1.append(["일","언론사","기사제목","업태","미리보기","기사 주소"])
+    while currentPage < lastPage:
+        i = 0
+        raw = requests.get("https://search.naver.com/search.naver?where=news&query="+keyword+"&sm=tab_opt&sort="+sort+"&photo=0&field=0&pd="+pd+"&ds="+ds+"&de="+de+"&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Aall&is_sug_officeid=1&start="+str(currentPage)+"1",
+                    headers={'User-Agent':'Mozilla/5.0'})
+        html = BeautifulSoup(raw.text, "html.parser")
+        articles = html.select("ul.list_news > li")
+        try: 
+            while i < 10:
+                checkResult = None
+                try:
+                    if (articles[i].select_one("i.spnew.ico_paper").text): 
+                        checkResult = "신문"
+                except: 
+                    pass
+                try:
+                    if (articles[i].select_one("i.spnew.api_ico_svideo").text): 
+                        checkResult = "방송"
+                except AttributeError:
+                    checkResult = "인터넷"
+                title = articles[i].select_one("a.news_tit").text
+                source = articles[i].select_one("a.info.press").text
+                sum = articles[i].select_one("a.api_txt_lines.dsc_txt_wrap").text
+                nlink = articles[i].select_one("a.news_tit")["href"]
+
+                source = source.replace("언론사 선정","")
+                print(checkResult)
+                print(source)
+                print(title)
+                print(sum)
+                print(nlink)
+                print('\n')
+
+                ws1.append([ds,source,title,checkResult,sum,nlink])
+                i += 1
+
+            currentPage = currentPage +1
+            if html.select_one('a.btn_next')["aria-disabled"] == "true":
+                print("마지막 페이지입니다.")
+            else:
+                lastPage = lastPage + 1
+                print(lastPage,"번째 페이지입니다.\n")
+        except IndexError:
+            print("마지막 기사입니다.")
+            break
+        wb.save(filename='crawlResult.xlsx')
+        
+croll()
