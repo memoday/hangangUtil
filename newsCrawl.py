6844bbeeb21371aac2269080b4c26e09a9375591
memoday <74040890+memoday@users.noreply.github.com>
2022-10-18 10:51:16 +0900

    범위 선택 기능 추가

--- newsCrawl.py
+++ newsCrawl.py
@@ -1,6 +1,8 @@
+import openpyxl
 import requests
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
+from openpyxl import workbook
+import datetime
 
 def getAttribute(articleIndex):
     if (articles[articleIndex].select_one("div.info_group > span:nth-child(2) > i.spnew.ico_paper")):
@@ -18,12 +20,11 @@ def getContents(articleIndex) -> tuple:
 
     return title, source, sum, nlink
 
-
-def main():
+def crawl(dateStart):
     setting = {
         "searchKeyword" : '한강', #검색 키워드
-        "dateStart" : "2022.10.16", #from date(ds)
-        "dateEnd" : "2022.10.16", #to date(de)
+        "dateStart" : dateStart, #from date(ds)
+        "dateEnd" : dateStart, #to date(de)
         'sort': "2", #오래된 순: 2
         'period': "3" #기간검색: 3  
     }
@@ -32,7 +33,7 @@ def main():
     # ws1.append(["일","언론사","기사제목","업태","미리보기","기사 주소"])
     while True:
         newsURL = "https://search.naver.com/search.naver?where=news&query="+setting['searchKeyword']+"&sm=tab_opt&sort="+setting['sort']+"&photo=0&field=0&pd="+setting['period']+"&ds="+setting['dateStart']+"&de="+setting['dateEnd']+"&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Aall&is_sug_officeid=1&start="+str(page)+"1"
-        
+
         raw = requests.get(newsURL, headers={'User-Agent':'Mozilla/5.0'})
         html = BeautifulSoup(raw.text, "html.parser")
 
@@ -57,10 +58,31 @@ def main():
 
         except IndexError:
             print("마지막 기사입니다.")
-            wb.save(filename='crawlResult.xlsx')
+            wb.save('crawlResult.xlsx')
             break
 
+def main ():
+    print('날짜 기간을 입력해주세요 dateStart')
+    dsY,dsM,dsD = map(int,input("시작 년.월.일을 입력해주세요(2022.10.12)").split('.'))
+    dateStart = datetime.datetime(dsY,dsM,dsD)
+    deY,deM,deD = map(int,input("종료 년.월.일을 입력해주세요(2022.10.12)").split('.'))
+    dateEnd = datetime.datetime(deY,deM,deD)
+
+    rawDays = dateEnd - dateStart
+    rangeDays = rawDays.days
+    print(rangeDays+1)
+    i = 0
+    for i in range(rangeDays+1):
+        urlDays = dateStart+ datetime.timedelta(days= i)
+        urlDays = str(urlDays.strftime('%Y.%m.%d'))
+        crawl(urlDays)
+        i += 1
+
+
 if __name__ == "__main__":
-    wb = Workbook()
+    try:
+        wb = openpyxl.load_workbook('crawlResult.xlsx')
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
     ws1 = wb.active
     main()
