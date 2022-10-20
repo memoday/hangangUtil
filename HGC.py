376d8abbb82d78ec43ba440ef7463b8b8914645b
memoday <74040890+memoday@users.noreply.github.com>
2022-10-20 10:59:41 +0900

    fileName modified
    fileName 함수 추가, 버그 수정

--- HGC.py
+++ HGC.py
@@ -53,11 +53,36 @@ def getContents(articleIndex) -> tuple:
 
     return title, source, sum, nlink
 
+def fileCreate(searchKeyword, sort,fileNameDays,self):
+
+    global wb, ws1, fileName
+
+    if sort == '0':
+        fileNameSort = "관련도순"
+    elif sort == '1':
+        fileNameSort = "최신순"
+    elif sort == '2':
+        fileNameSort =  "오래된순"
+
+    fileName = searchKeyword+'_'+fileNameDays+'_'+fileNameSort+'.xlsx'
+
+    try:
+        wb = openpyxl.load_workbook(fileName)
+        self.label_main.setText("File Exists")
+        self.label_main.setStyleSheet("Color: Red")
+        return 'exists'
+    except FileNotFoundError:
+        print('FileNotFound: 엑셀 파일을 새로 생성합니다.')
+        wb = openpyxl.Workbook()            
+    ws1 = wb.active
+
+
 def crawl(searchKeyword, dateStart, sort, self):
+
     setting = {
         "searchKeyword" : searchKeyword, #검색 키워드
         "dateStart" : dateStart, #from date(ds)
-        "dateEnd" : dateStart, #to date(de)
+        "dateEnd" : dateStart, #to date(de), 날짜 정보를 받아오지 못해 일자별로 crawl()작업을 수행함
         'sort': sort, #오래된 순: 2 #최신 순: 1 #관련도 순: 0
         'period': "3" #사용자 설정 기간검색: 3  
         #전체: 0, 1시간~6시간: 7~12, 1일: 4, 1주: 1, 1개월: 2, 3개월: 13, 6개월: 6, 1년: 5
@@ -65,6 +90,7 @@ def crawl(searchKeyword, dateStart, sort, self):
     
     page = 0
     # ws1.append(["일","언론사","기사제목","업태","미리보기","기사 주소"])
+    
     while True:
         newsURL = "https://search.naver.com/search.naver?where=news&query="+setting['searchKeyword']+"&sm=tab_opt&sort="+setting['sort']+"&photo=0&field=0&pd="+setting['period']+"&ds="+setting['dateStart']+"&de="+setting['dateEnd']+"&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Aall&is_sug_officeid=1&start="+str(page)+"1"
 
@@ -78,10 +104,10 @@ def crawl(searchKeyword, dateStart, sort, self):
             self.label_main.setText("Not Found")
             self.label_main.setStyleSheet("Color: Red")
             break
-        if checkNextPage == "false":
-            print(page+1,"번째 페이지입니다.\n")
+        if checkNextPage == "true":
+            print(page+1,"번째 페이지입니다.")
         else:
-            print(page+1,"번째 마지막 페이지입니다.")
+            print(page+1,"번째 페이지입니다.")
 
         global articles
         articles = html.select("ul.list_news > li")
@@ -91,14 +117,14 @@ def crawl(searchKeyword, dateStart, sort, self):
                 title, source, sum, nlink = getContents(articleIndex)
 
                 data = [setting['dateStart'], source, title, attribute, sum, nlink]
-                print(attribute, '\n', source, '\n', title, '\n\n', sum, '\n', nlink, '\n')
+                # print(attribute, '\n', source, '\n', title, '\n\n', sum, '\n', nlink, '\n')
 
                 ws1.append(data)
             page += 1
 
         except IndexError:
             print("마지막 기사입니다.")
-            wb.save('crawlResult.xlsx')
+            wb.save(fileName)
             break
         self.label_main.setText("Success")
         self.label_main.setStyleSheet("Color: Green")
@@ -139,11 +165,19 @@ class WindowClass(QMainWindow, form_class):
 
             rangeDays = getRange(dateStart,dateEnd)
 
+            fileNameDays = dateStart+'~'+dateEnd #파일명에 들어갈 날짜
+
             dsY,dsM,dsD = map(int,dateStart.split('.'))
             dateStart = datetime.datetime(dsY,dsM,dsD) 
             #dateStart를 str에서 datetime으로 type 변경, 날짜 계산하기 위한 과정
 
             sort = str(self.combo_sort.currentIndex()) #combo box안에 있는 값 전달
+
+            fileCreate(searchKeyword,sort,fileNameDays,self)
+            file = fileCreate(searchKeyword,sort,fileNameDays,self)
+
+            if file == 'exists':
+                return
 
             i = 0
 
@@ -159,13 +193,6 @@ class WindowClass(QMainWindow, form_class):
 
 
 if __name__ == "__main__":
-    try:
-        print('crawlResult.xlsx 파일이 확인되었습니다')
-        wb = openpyxl.load_workbook('crawlResult.xlsx')
-    except FileNotFoundError:
-        print('FileNotFound: crawlResult.xlsx을 찾지 못해 새로 생성합니다.')
-        wb = openpyxl.Workbook()
-    ws1 = wb.active
     app = QApplication(sys.argv) 
     myWindow = WindowClass() 
     myWindow.show()
