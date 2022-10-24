6f35b49f14615031ad726461c98a4736c83e314d
memoday <74040890+memoday@users.noreply.github.com>
2022-10-24 15:00:27 +0900

    bug fixed

--- checkNews.py
+++ checkNews.py
@@ -4,7 +4,7 @@ import re
 
 def get_real_url_from_shortlink(url): #단축링크 원본링크로 변경
     resp = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
-    print(resp.url)
+    print('Original URL:'+resp.url)
     return resp.url
 
 def checkNews(url) -> tuple : #언론사별 selector
@@ -30,14 +30,16 @@ def checkNews(url) -> tuple : #언론사별 selector
         title = source.select_one("#content > div > div.content > div > div.news_headline > h4").text
         press = source.select_one("#pressLogo > a > img")['alt']
         content = source.select_one("#newsEndContents")
-        content.select_one('p').decompose()
-        content.select_one('p').decompose()
-        content.select_one('div').decompose()
-        content.select_one('div').decompose()
-        content.select_one('div').decompose()
-        content.select_one('div').decompose()
+
+        content.find(class_ = 'source').decompose()
+        content.find(class_ = 'byline').decompose()
+        content.find(class_ = 'reporter_area').decompose()
+        content.find(class_ = 'copyright').decompose()
+        content.find(class_ = 'categorize').decompose()
+        content.find(class_ = 'promotion').decompose()
 
         date = source.select_one("#content > div > div.content > div > div.news_headline > div > span:nth-child(1)").text
+        
         contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
         contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
         contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
@@ -45,7 +47,7 @@ def checkNews(url) -> tuple : #언론사별 selector
         contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
 
     if "newspim.com" in url: #뉴스핌
-        print('newspim checked')
+        print('newspim.com checked')
         title = source.select_one("#main-title").text
         press = "뉴스핌"
         content = source.select_one("#wrap > div.container.subwrap > div > div:nth-child(2) > div.left > div > div.contents")
@@ -56,7 +58,36 @@ def checkNews(url) -> tuple : #언론사별 selector
         contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
         to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
         contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
+    
+    # if "hg-times.com" in url: #한강타임즈
+    #     print('hg-times.com checked')
+    #     title = source.select_one('#user-container > div.float-center.max-width-1100 > header > header > div').text
+    #     press ='한강타임즈'
+    #     content = source.select_one('div.articleBody').text
 
+    #     try:
+    #         content.find(type = 'text/javascript').decompose()
+    #         content.find(class_ = 'tag-group').decompose()
+    #         content.find(class_ = 'article-body-dn-txt auto-marbtm-50 no-bullet').decompose()
+    #         content.find(class_ = 'view-copyright').decompose()
+    #         content.find('style').decompose()
+    #         content.find(class_ = 'hw-box').decompose()
+    #         content.find(class_ = 'emoji-tit').decompose()
+    #         content.find(id = 'emoji-for').decompose()
+    #         content.find(class_ = 'editor-profile').decompose()
+    #         content.find(class_ ='clearfix user-sns-send').decompose()
+    #         content.find(class_ ='clearfix  ').decompose()
+    #         content.find(class_ ='clearfix user-sns-send').decompose()
+    #     except AttributeError:
+    #         pass
+
+    #     date = source.select_one("#user-container > div.float-center.max-width-1100 > header > section > div.info-text > ul > li:nth-child(2)").text
+    #     contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
+    #     contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
+    #     to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
+    #     contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
+        
+        
     else:
         print("호환되지 않는 언론사입니다")
     return title,press,contentEdited,date
