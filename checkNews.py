3b1282d983dc21338edb6369c6ed3a2cf1776cb8
memoday <74040890+memoday@users.noreply.github.com>
2022-12-14 13:27:47 +0900

    HGA fixed
    [사진] 태그 제거, 언론사 수동 설정 추가

--- checkNews.py
+++ checkNews.py
@@ -24,7 +24,7 @@ def checkNews(url) -> tuple : #언론사별 selector
         date = source.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span").text
         contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
         contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-        contentStr = contentStr.replace('</img>','[사진]') #이미지 위치 확인
+        contentStr = contentStr.replace('</img>','') #이미지 위치 확인
         to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
         contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
 
@@ -46,7 +46,7 @@ def checkNews(url) -> tuple : #언론사별 selector
         
         contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
         contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-        contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
+        contentStr = contentStr.replace('</img>','') #이미지 위치 확인
         to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
         contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
     
@@ -59,13 +59,9 @@ def checkNews(url) -> tuple : #언론사별 selector
 
         contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
         contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-        contentStr = contentStr.replace('</img>','[사진]') #이미지 위치 확인
+        contentStr = contentStr.replace('</img>','') #이미지 위치 확인
         to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
         contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
-
-
-
-
 
     # elif "newspim.com" in url: #뉴스핌
     #     print('newspim.com checked')
@@ -76,35 +72,6 @@ def checkNews(url) -> tuple : #언론사별 selector
     #     date = date.replace("년",".").replace("월",".").replace("일","")
     #     contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
     #     contentStr = str(contentStr).replace('</table>','\n') #이미지 부연설명 내용과 분리
-    #     contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
-    #     to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
-    #     contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
-    
-    
-    # elif "hg-times.com" in url: #한강타임즈
-    #     print('hg-times.com checked')
-    #     title = source.select_one('#user-container > div.float-center.max-width-1100 > header > header > div').text
-    #     press ='한강타임즈'
-    #     content = source.select_one('div.articleBody').text
-
-    #     try:
-    #         content.find(type = 'text/javascript').decompose()
-    #         content.find(class_ = 'tag-group').decompose()
-    #         content.find(class_ = 'article-body-dn-txt auto-marbtm-50 no-bullet').decompose()
-    #         content.find(class_ = 'view-copyright').decompose()
-    #         content.find('style').decompose()
-    #         content.find(class_ = 'hw-box').decompose()
-    #         content.find(class_ = 'emoji-tit').decompose()
-    #         content.find(id = 'emoji-for').decompose()
-    #         content.find(class_ = 'editor-profile').decompose()
-    #         content.find(class_ ='clearfix user-sns-send').decompose()
-    #         content.find(class_ ='clearfix  ').decompose()
-    #         content.find(class_ ='clearfix user-sns-send').decompose()
-    #     except AttributeError:
-    #         pass
-
-    #     date = source.select_one("#user-container > div.float-center.max-width-1100 > header > section > div.info-text > ul > li:nth-child(2)").text
-    #     contentStr = str(content).replace('<br/>','\n') #<br>태그 Enter키로 변경
     #     contentStr = contentStr.replace('</img>','[사진]\n') #이미지 위치 확인
     #     to_clean = re.compile('<.*?>') # <> 사이에 있는 것들
     #     contentEdited = re.sub(to_clean,'',contentStr) #html태그 모두 지우기
@@ -140,6 +107,8 @@ def checkNews(url) -> tuple : #언론사별 selector
                 press = '기호일보'
             elif '글로벌 뉴스 미디어 채널 데일리포스트' in press:
                 press = '데일리포스트'
+            elif 'www.donga.com' in press:
+                press = '동아일보'
 
 
             
