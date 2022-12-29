0a06ce278d59c7d3d638ade4d288a9130f6f9601
Yoon <74040890+memoday@users.noreply.github.com>
2022-12-29 14:47:12 +0900

    Selenium Ver. Update

--- hanglShorten.py
+++ hanglShorten.py
@@ -1,26 +1,15 @@
 import time
 from selenium import webdriver
-import os
-import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_argument('User-Agent= Mozilla/5.0')
-options.add_argument("--disable-extensions")
-options.add_argument("disable-infobars")
-options.add_argument("disable-gpu")
-
 options.add_argument('headless') #크롬창 표시 금지
 
-chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
-driver_path = f'./{chrome_ver}/chromedriver.exe'
-if os.path.exists(driver_path):
-    print(f"chromedriver is installed: {driver_path}")
-else:
-    print('installing chromedriver')
-    chromedriver_autoinstaller.install(cwd=True) #chromedriver 크롬 버전에 맞춰 설치
 
-    
-driver = webdriver.Chrome(options=options, executable_path=driver_path)
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 driver.get("https://han.gl")
 
 
@@ -28,11 +17,11 @@ def hanglShorten(longUrl):
     if "han.gl" in longUrl:
         return longUrl
 
-    driver.find_element_by_id('url').send_keys(longUrl)
-    driver.find_element_by_xpath('/html/body/section[1]/div/div/div/div/form/div/div/button[2]').click()
+    driver.find_element(By.ID,'url').send_keys(longUrl)
+    driver.find_element(By.XPATH,'/html/body/section[1]/div/div/div/div/form/div/div/button[2]').click()
 
     for i in range(30):
-        shortenUrl = driver.find_element_by_xpath('/html/body/section[1]/div/div/div/div/form/div/div/button[1]')
+        shortenUrl = driver.find_element(By.XPATH,'/html/body/section[1]/div/div/div/div/form/div/div/button[1]')
         shortenUrl = shortenUrl.get_attribute('data-clipboard-text')
         if shortenUrl != None:
             print('None 탈출')
